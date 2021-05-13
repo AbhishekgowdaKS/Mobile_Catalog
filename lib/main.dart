6315d03a954761b116fc0ebe62ddbdcffbd11adb
Abhishekgowda K S <abhishek352mlv@gmail.com>
2021-05-13 22:36:14 +0530

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: "Mobile Catalog",
       home: Material(
         child: Center(
           child: Container(
