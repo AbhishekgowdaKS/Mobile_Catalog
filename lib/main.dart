435359aa7961b748299dcac4471106a1587dda22
Abhishekgowda K S <abhishek352mlv@gmail.com>
2021-05-14 17:59:51 +0530

    added login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mobile_cataloge/pages/home_page.dart';
+import 'package:mobile_cataloge/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,15 +13,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Material Catalog",
       debugShowCheckedModeBanner: false,
-      title: "Mobile Catalog",
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Mobile Catalog"),
-          ),
-        ),
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.lato().fontFamily,
       ),
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
